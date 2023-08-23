c130950382e6eecee8b9d1b54dcf382db8d6e688
Jack Freeman <jfreeman@morgridge.org>
2023-08-23 14:07:04 -0500

    after Ron suggestions

--- abstract_comprehension.py
+++ abstract_comprehension.py
@@ -12,18 +12,22 @@ import skim_no_km as skim
 # Step 1: Constants and Configuration
 def get_config():
     return {
+        # things that may never change
         "PORT": "5081",
         "API_URL": f"http://localhost:5081/skim/api/jobs",
-        "A_TERM": "Crohn's disease",
-        "C_TERMS_FILE": "FDA_approved_ProductsActiveIngredientOnly_DupsRemovedCleanedUp.txt",
-        "B_TERMS_FILE": "BIO_PROCESS_cleaned.txt",
-        "OUTPUT_JSON": "test_errything_updated.json",
         "API_KEY": os.getenv("OPENAI_API_KEY"),
         "RATE_LIMIT": 3,
         "DELAY": 10,
         "BASE_URL": "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi",
         "PUBMED_PARAMS": {"db": "pubmed", "retmode": "xml", "rettype": "abstract"},
-        "MAX_ABSTRACTS": 10,
+        # things that may change
+        "A_TERM": "Crohn's disease",
+        "C_TERMS_FILE": "FDA_approved_ProductsActiveIngredientOnly_DupsRemovedCleanedUp.txt",
+        "B_TERMS_FILE": "BIO_PROCESS_cleaned.txt",
+        "OUTPUT_JSON": "test_errything_updated.json",
+        "MAX_ABSTRACTS": 5,
+        "SORT_COLUMN": "bc_sort_ratio",
+        "NUM_C_TERMS": 3,
         "JOB_SETTINGS": {
             "skim": {
                 "ab_fet_threshold": 1e-5,
@@ -63,6 +67,21 @@ def fetch_abstract_from_pubmed(pmid, base_url, params):
     return abstract_text, paper_url
 
 
+def test_openai_connection(api_key):
+    openai.api_key = api_key
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "You are a medical research analyst."},
+                {"role": "user", "content": "Test connection to OpenAI."},
+            ],
+        )
+        print("Successfully connected to OpenAI!")
+    except Exception as e:
+        print(f"Failed to connect to OpenAI. Error: {e}")
+
+
 def analyze_abstract_with_gpt4(
     consolidated_abstracts, b_term, a_term, abstracts_separate, api_key
 ):
@@ -70,6 +89,9 @@ def analyze_abstract_with_gpt4(
     if abstracts_separate:
         responses = []
         for abstract in consolidated_abstracts:
+            assert abstract and len(abstract) > 100, "Abstract is empty or too short"
+            assert b_term, "B term is empty"
+            assert a_term, "A term is empty"
             prompt = (
                 f"Read the following abstract and classify the discussed treatment: {b_term} "
                 f"as useful, potentially useful, ineffective, potentially harmful, or harmful for "
@@ -89,6 +111,7 @@ def analyze_abstract_with_gpt4(
                 temperature=0,
                 max_tokens=512,
             )
+            assert response["choices"][0]["message"]["content"], "Response is empty"
             responses.append(response["choices"][0]["message"]["content"])
         return responses
     else:
@@ -112,6 +135,7 @@ def analyze_abstract_with_gpt4(
 
 # Step 4: Data Manipulation and Analysis
 def process_abstracts(pmids, rate_limit, delay, base_url, params):
+    assert pmids, "List of PMIDs is empty"
     pmid_batches = [pmids[i : i + rate_limit] for i in range(0, len(pmids), rate_limit)]
     consolidated_abstracts = []
     paper_urls = []
@@ -127,6 +151,7 @@ def process_abstracts(pmids, rate_limit, delay, base_url, params):
 
 def process_single_row(row, config):
     pmids = eval(row["ab_pmid_intersection"])
+    assert pmids, "No PMIDs found in the row"
     pmids = pmids[: config["MAX_ABSTRACTS"]]
 
     consolidated_abstracts, paper_urls = process_abstracts(
@@ -136,7 +161,6 @@ def process_single_row(row, config):
         config["BASE_URL"],
         config["PUBMED_PARAMS"],
     )
-    consolidated_abstracts = " ".join(consolidated_abstracts)
     b_term = row["b_term"]
     result = analyze_abstract_with_gpt4(
         consolidated_abstracts, b_term, config["A_TERM"], True, config["API_KEY"]
@@ -152,17 +176,28 @@ def process_single_row(row, config):
 # Step 5: Main Workflow
 def main_workflow():
     config = get_config()
+
+    assert config, "Configuration is empty or invalid"
     a_term = config["A_TERM"]
+    assert a_term, "A_TERM is not defined in the configuration"
 
-    df = read_tsv_to_dataframe(skim.main_workflow(a_term, config))
-    results_list = []
+    try:
+        df = read_tsv_to_dataframe(skim.main_workflow(a_term, config))
+        assert not df.empty, "The dataframe is empty"
 
-    for _, row in df.iterrows():
-        result_dict = process_single_row(row, config)
-        results_list.append(result_dict)
+        results_list = []
+        test_openai_connection(config["API_KEY"])
+        for index, row in df.iterrows():
+            result_dict = process_single_row(row, config)
+            results_list.append(result_dict)
+            print(f"Processed row {index + 1} ({row['b_term']}) of {len(df)}")
 
-    write_to_json(results_list, config["OUTPUT_JSON"])
-    print(f"Analysis results have been saved to {config['OUTPUT_JSON']}")
+        assert results_list, "No results were processed"
+        write_to_json(results_list, config["OUTPUT_JSON"])
+        print(f"Analysis results have been saved to {config['OUTPUT_JSON']}")
+
+    except Exception as e:
+        print(f"Error occurred during processing: {e}")
 
 
 if __name__ == "__main__":
--- skim_no_km.py
+++ skim_no_km.py
@@ -4,79 +4,73 @@ import requests
 import time
 
 
-# Step 1: Constants and Configuration
-def get_config():
-    return {
-        "PORT": "5081",
-        "API_URL": f"http://localhost:5081/skim/api/jobs",
-        "A_TERM": "Crohn's disease",
-        "C_TERMS_FILE": "FDA_approved_ProductsActiveIngredientOnly_DupsRemovedCleanedUp.txt",
-        "B_TERMS_FILE": "BIO_PROCESS_cleaned.txt",
-    }
-
-
-config = get_config()
-
-
-# Step 2: File Operations
+# File Operations
 def read_terms_from_file(filename):
+    """Read terms from a given file and return them as a list."""
     with open(filename, "r") as f:
         terms = [line.strip() for line in f]
     return terms
 
 
 def save_to_tsv(data, filename):
+    """Save the data into a TSV (Tab Separated Values) file."""
     df = pd.DataFrame(data)
     df.to_csv(filename, sep="\t", index=False)
 
 
-# Step 3: API Calls
+# API Calls
 def post_api_request(url, payload, username, password):
+    """Send a POST request to the API and return the JSON response."""
     response = requests.post(url, json=payload, auth=(username, password))
+    response.raise_for_status()
     return response.json()
 
 
 def get_api_request(url, job_id, username, password):
+    """Send a GET request to the API and return the JSON response."""
     response = requests.get(f"{url}?id={job_id}", auth=(username, password))
+    response.raise_for_status()
     return response.json()
 
 
 def wait_for_job_completion(url, job_id, username, password):
+    """Wait for an API job to complete and return the result."""
     while True:
         response_json = get_api_request(url, job_id, username, password)
         job_status = response_json["status"]
         if job_status in ["finished", "failed"]:
             break
         time.sleep(5)
+    assert "result" in response_json, "Job did not complete successfully."
     return response_json.get("result", None)
 
 
 def run_api_query(payload, url, username, password):
+    """Initiate an API query and wait for its completion."""
     initial_response = post_api_request(url, payload, username, password)
     job_id = initial_response["id"]
     return wait_for_job_completion(url, job_id, username, password)
 
 
-# Step 4: Job Configuration
+# Job Configuration
 def configure_job(
     job_type, a_term, c_terms, b_terms=None, filtered_terms=None, config=None
 ):
-    if not config:
-        config = get_config()  # If no config passed, use the default configuration
+    """Configure a job based on the provided type and terms."""
+    assert config, "No configuration provided"
 
     common_settings = {
         "a_terms": [a_term],
         "return_pmids": True,
         "query_knowledge_graph": False,
     }
-
     job_specific_settings = config["JOB_SETTINGS"].get(job_type, {})
 
     if job_type == "skim":
         c_terms_filtered = list(set(c_terms) - set(filtered_terms))
         return {
             **common_settings,
-            **job_specific_settings,  # Unpack the job-specific settings here
+            **job_specific_settings,
             "b_terms": b_terms,
             "c_terms": c_terms_filtered,
             "top_n_articles": 20,
@@ -96,10 +90,10 @@ def configure_job(
             "top_n_articles": 10,
         }
     else:
-        raise ValueError("Invalid job type")
+        raise ValueError(f"Invalid job type: {job_type}")
 
 
-# Step 5: Main Workflow
+# Main Workflow
 def run_and_save_query(
     job_type,
     a_term,
@@ -108,49 +102,60 @@ def run_and_save_query(
     filtered_terms=None,
     username="username",
     password="password",
-    config=None,  # Add the config parameter here
+    config=None,
 ):
+    """Run a query, save the results, and return the saved file path."""
     job_config = configure_job(
         job_type, a_term, c_terms, b_terms, filtered_terms, config
     )
-    result = run_api_query(
-        job_config, config["API_URL"], username, password
-    )  # Ensure config is used here
+    result = run_api_query(job_config, config["API_URL"], username, password)
     file_path = f'{job_type}_{job_config["a_terms"][0]}_output.tsv'
     save_to_tsv(result, file_path)
     return file_path
 
 
 def main_workflow(a_term, config=None):
+    """Execute the main workflow for processing terms."""
+    assert config, "No configuration provided"
+
+    print("Reading terms from files...")
     c_terms = read_terms_from_file(config["C_TERMS_FILE"])
     b_terms = read_terms_from_file(config["B_TERMS_FILE"])
 
-    km_file_path = run_and_save_query(
-        "first_km", a_term, c_terms, config=config
-    )  # Pass the config here
+    print("Running and saving first KM query to subset...")
+    km_file_path = run_and_save_query("first_km", a_term, c_terms, config=config)
 
     km_df = pd.read_csv(km_file_path, sep="\t")
     km_filtered_terms = km_df["b_term"].tolist()
 
+    print("Running and saving SKIM query...")
     skim_file_path = run_and_save_query(
-        "skim",
-        a_term,
-        c_terms,
-        b_terms,
-        km_filtered_terms,
-        config=config,  # Pass the config here
+        "skim", a_term, c_terms, b_terms, km_filtered_terms, config=config
     )
 
+    print("Processing SKIM results...")
     skim_df = pd.read_csv(skim_file_path, sep="\t")
+    sort_column = config.get("SORT_COLUMN", "bc_sort_ratio")
+    skim_df = skim_df.sort_values(by=sort_column, ascending=False)
+    assert not skim_df.empty, "SKIM results are empty"
+
     unique_c_terms = skim_df["c_term"].unique().tolist()
 
+    print("Running and saving final KM query...")
     final_km_file_path = run_and_save_query(
         "final_km", a_term, unique_c_terms, config=config
-    )  # Pass the config here
+    )
 
-    print(f"Final KM query results saved to {final_km_file_path}")
-    return final_km_file_path
+    final_km_df = pd.read_csv(final_km_file_path, sep="\t")
 
+    # Filter final_km_df to get rows where 'ab_pmid_intersection' is not empty
+    # until number of rows equals NUM_C_TERMS
+    valid_rows = final_km_df[final_km_df["ab_pmid_intersection"].notna()]
+    final_km_df_filtered = valid_rows.head(config["NUM_C_TERMS"])
 
-if __name__ == "__main__":
-    main_workflow(config["A_TERM"])
+    # Save the filtered final_km_df to disk and return its path
+    filtered_file_path = os.path.splitext(final_km_file_path)[0] + "_filtered.tsv"
+    final_km_df_filtered.to_csv(filtered_file_path, sep="\t", index=False)
+
+    print(f"Filtered final KM query results saved to {filtered_file_path}")
+    return filtered_file_path
